eecf2839abcd47f66b9c647214bd4986fffb7f86
Sonjii99x-Glitch <sonjii99x@gmail.com>
2026-03-29 07:36:40 +0000

    Fix master installer paths to call installers correctly

--- installers/master_installer.bat
+++ installers/master_installer.bat
@@ -3,13 +3,13 @@ set /p OPI_IP=Enter Orange Pi Server IP:
 set /p PASSWORD=Enter OPI root password:
 
 echo Installing PisoNet Server...
-call installer_server.bat %OPI_IP% %PASSWORD%
+call installers\installer_server.bat %OPI_IP% %PASSWORD%
 
 echo Installing Windows Client...
-call installer_client.bat %OPI_IP% %PASSWORD%
+call installers\installer_client.bat %OPI_IP% %PASSWORD%
 
 echo Installing Admin Dashboard...
-call installer_admin.bat %OPI_IP% %PASSWORD%
+call installers\installer_admin.bat %OPI_IP% %PASSWORD%
 
 echo All installations complete.
 pause
